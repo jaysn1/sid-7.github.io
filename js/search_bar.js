79eed3f73c698f6f48537f1a9ca557e6c4174913
monilnisar4900 <monilnisar4900@gmail.com>
2020-04-16 17:18:34 -0700

    Code in integrated pretty tightly. Many tiny changes left.

--- js/search_bar.js
+++ js/search_bar.js
@@ -3,24 +3,26 @@ var len = list.length;
 
 function addToList(){
       var x = document.getElementById("myInput");
-      list.push(x.value);
+      list.push(x.value.toLowerCase());
 
       var button = document.createElement("button");
       button.classList.add("button");
       button.innerHTML = x.value;
 
       // 2. Append somewhere
-      var body = document.getElementsByTagName("body")[0];
+      var body = document.getElementById("search bar forum");
       body.appendChild(button);
 
       button.addEventListener ("click", function() {
-      body.removeChild(button);
-      document.getElementById("add_button").classList.remove("disabled");
-      var index = list.indexOf(button.innerHTML);
-      if (index !== -1) list.splice(index, 1);
+        body.removeChild(button);
+        document.getElementById("add_button").classList.remove("disabled");
+        var index = list.indexOf(button.innerHTML.toLowerCase());
+        console.log(index)
+        if (index !== -1) list.splice(index, 1);
       })
 
       if (list.length>=3) document.getElementById("add_button").classList.add("disabled");
+      console.log(list);
 }
 
 function autocomplete(inp, arr) {
@@ -121,13 +123,8 @@ function autocomplete(inp, arr) {
 }
 
 function generateVisualization(){
-  var K = 3;
-  if(list.length<K){
-    for(i=0;i<K-list.length;i++){
-      list.length.push("");
-    }
-  }
-  
+  entered_symptoms = list;
+  refreshVisualization();
 }
 
 // var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
